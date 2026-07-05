a5393bd743f1e47007a6de45309d8a1c1f2ec18d
jeddytech <ugbojs@gmail.com>
2026-07-05 17:34:58 +0100

    Refactor Firebase imports and exports

--- firebase.js
+++ firebase.js
@@ -1,17 +1,26 @@
+// Firebase App
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
+// Firestore
 import {
   getFirestore,
   collection,
+  doc,
   addDoc,
+  setDoc,
   getDoc,
   getDocs,
-  doc,
+  updateDoc,
+  deleteDoc,
   query,
+  where,
   orderBy,
+  limit,
+  onSnapshot,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
+// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCVFWQF7P-9ZQT4kak1i_UICXlmNd1qeCE",
   authDomain: "jdwishlink.firebaseapp.com",
@@ -21,17 +30,27 @@ const firebaseConfig = {
   appId: "1:364819369371:web:a1162a4b62cf89f50c253e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firestore Database
 const db = getFirestore(app);
 
+// Export Firestore
 export {
   db,
   collection,
+  doc,
   addDoc,
+  setDoc,
   getDoc,
   getDocs,
-  doc,
+  updateDoc,
+  deleteDoc,
   query,
+  where,
   orderBy,
+  limit,
+  onSnapshot,
   serverTimestamp
 };
