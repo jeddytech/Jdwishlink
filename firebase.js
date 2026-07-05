c3c94e00cda76a8048c984e2a08cc079a886a91b
jeddytech <ugbojs@gmail.com>
2026-07-05 17:16:04 +0100

    Reorder imports and export statements in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,17 @@
-// Firebase App
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-// Firestore Database
 import {
   getFirestore,
   collection,
   addDoc,
-  getDocs,
   getDoc,
+  getDocs,
   doc,
   query,
-  where,
   orderBy,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-// Firebase Configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCVFWQF7P-9ZQT4kak1i_UICXlmNd1qeCE",
   authDomain: "jdwishlink.firebaseapp.com",
@@ -25,22 +21,17 @@ const firebaseConfig = {
   appId: "1:364819369371:web:a1162a4b62cf89f50c253e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
-// Export everything we'll use
 export {
   db,
   collection,
   addDoc,
-  getDocs,
   getDoc,
+  getDocs,
   doc,
   query,
-  where,
   orderBy,
   serverTimestamp
 };
